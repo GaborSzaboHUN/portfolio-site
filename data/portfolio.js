4e78394f09ec4ccb0f6f3a93fe88f1158fee9520
GaborSzaboHUN <66555347+GaborSzaboHUN@users.noreply.github.com>
2022-12-04 23:51:55 +0100

    Shortened the portfolio section with real projects

--- data/portfolio.js
+++ data/portfolio.js
@@ -1,38 +1,6 @@
 const portfolio = [
     {
         "id": "1",
-        "title": "1. item",
-        "details": "1. item's details. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mattis pellentesque id nibh tortor id aliquet lectus proin.",
-        "imgLink": "/assets/img/austin-schmid-0asZ7HcEHSQ-unsplash.jpg",
-        "liveProject": "#",
-        "gitLink": "#"
-    },
-    {
-        "id": "2",
-        "title": "2. item",
-        "details": "2. item's details. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mattis pellentesque id nibh tortor id aliquet lectus proin.",
-        "imgLink": "/assets/img/dave-goudreau-OxtLUqXKW-Y-unsplash.jpg",
-        "liveProject": "#",
-        "gitLink": "#"
-    },
-    {
-        "id": "3",
-        "title": "3. item",
-        "details": "3. item's details. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mattis pellentesque id nibh tortor id aliquet lectus proin.",
-        "imgLink": "/assets/img/eugene-golovesov-f6lbUDprkoQ-unsplash.jpg",
-        "liveProject": "#",
-        "gitLink": "#"
-    },
-    {
-        "id": "4",
-        "title": "4. item",
-        "details": "4. item's details. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mattis pellentesque id nibh tortor id aliquet lectus proin.",
-        "imgLink": "/assets/img/ian-talmacs-pe6U_FqAXvU-unsplash.jpg",
-        "liveProject": "#",
-        "gitLink": "#"
-    },
-    {
-        "id": "5",
         "title": "Art Gallery with React",
         "details": "With this project my goal was to create a page summarizing selected exhibitions of a fictional gallery. I fetched the data from the public API of the MET museum. This project is made with <strong>HTML</strong>, <strong>CSS</strong> and <strong>React</strong>.",
         "imgLink": "/assets/img/janmesh-shah-EXyVT3q4J_8-unsplash.jpg",
@@ -40,7 +8,7 @@ const portfolio = [
         "gitLink": "https://github.com/GaborSzaboHUN/art-galery-React"
     },
     {
-        "id": "6",
+        "id": "2",
         "title": "Weather app with React",
         "details": "My first weather application made with react. Made as part of a team effort. I wanted to improve the previous JS version with some extra features. This project is made with <strong>HTML</strong>, <strong>CSS</strong> and <strong>React</strong>.",
         "imgLink": "/assets/img/kellen-riggin-G9SJLINNdcA-unsplash.jpg",
@@ -48,7 +16,7 @@ const portfolio = [
         "gitLink": "https://github.com/GaborSzaboHUN/weather-App-React"
     },
     {
-        "id": "7",
+        "id": "3",
         "title": "Weather app with JS",
         "details": "My first weather app that I made with JS. My goal in the project was to automatically offer results from a list stored in a JSON file in case of a search, and to store the favorites at the top of the existing drop-down list. This project is made with <strong>HTML</strong>, <strong>CSS</strong> and <strong>JS</strong>.",
         "imgLink": "/assets/img/nantu-das-wYJM9PCbURg-unsplash.jpg",
@@ -56,7 +24,7 @@ const portfolio = [
         "gitLink": "https://github.com/GaborSzaboHUN/weather-App-JS"
     },
     {
-        "id": "8",
+        "id": "4",
         "title": "Image of the day by NASA",
         "details": "In this project, I display the image of the day by NASA with <strong>fetch</strong>. Using the calendar, you can also view photos and videos from previous days. This project is made with <strong>HTML</strong>, <strong>CSS</strong> and <strong>JS</strong>.",
         "imgLink": "/assets/img/wolfgang-hasselmann--AuNuqFUPQ8-unsplash.jpg",
@@ -64,7 +32,7 @@ const portfolio = [
         "gitLink": "https://github.com/GaborSzaboHUN/ask-nasa-javascript-GaborSzaboHUN"
     },
     {
-        "id": "9",
+        "id": "5",
         "title": "Build a Behance UI/UX/Web project",
         "details": "I have created this project to practice UI build. I looked for a nice web design on Behance and built it using <strong>HTML</strong> and <strong>CSS</strong> and added small effects.",
         "imgLink": "/assets/img/wolfgang-hasselmann-jl3bltYgs8c-unsplash.jpg",
