0d30022730981c8308be521b14dc0237c02fdc6a
GaborSzaboHUN <66555347+GaborSzaboHUN@users.noreply.github.com>
2022-12-07 00:50:05 +0100

    Update portfolio with images

--- data/portfolio.js
+++ data/portfolio.js
@@ -3,7 +3,7 @@ const portfolio = [
         "id": "1",
         "title": "Art Gallery with React",
         "details": "With this project my goal was to create a page summarizing selected exhibitions of a fictional gallery. I fetched the data from the public API of the MET museum. This project is made with <strong>HTML</strong>, <strong>CSS</strong> and <strong>React</strong>.",
-        "imgLink": "/assets/img/janmesh-shah-EXyVT3q4J_8-unsplash.jpg",
+        "imgLink": "/assets/img/Gagarin-gallery.png",
         "liveProject": "#",
         "gitLink": "https://github.com/GaborSzaboHUN/art-galery-React"
     },
@@ -11,7 +11,7 @@ const portfolio = [
         "id": "2",
         "title": "Weather app with React",
         "details": "My first weather application made with react. Made as part of a team effort. I wanted to improve the previous JS version with some extra features. This project is made with <strong>HTML</strong>, <strong>CSS</strong> and <strong>React</strong>.",
-        "imgLink": "/assets/img/kellen-riggin-G9SJLINNdcA-unsplash.jpg",
+        "imgLink": "/assets/img/WeatherApp-React.png",
         "liveProject": "#",
         "gitLink": "https://github.com/GaborSzaboHUN/weather-App-React"
     },
@@ -19,7 +19,7 @@ const portfolio = [
         "id": "3",
         "title": "Weather app with JS",
         "details": "My first weather app that I made with JS. My goal in the project was to automatically offer results from a list stored in a JSON file in case of a search, and to store the favorites at the top of the existing drop-down list. This project is made with <strong>HTML</strong>, <strong>CSS</strong> and <strong>JS</strong>.",
-        "imgLink": "/assets/img/nantu-das-wYJM9PCbURg-unsplash.jpg",
+        "imgLink": "/assets/img/WeatherApp-JS.png",
         "liveProject": "#",
         "gitLink": "https://github.com/GaborSzaboHUN/weather-App-JS"
     },
@@ -27,7 +27,7 @@ const portfolio = [
         "id": "4",
         "title": "Image of the day by NASA",
         "details": "In this project, I display the image of the day by NASA with <strong>fetch</strong>. Using the calendar, you can also view photos and videos from previous days. This project is made with <strong>HTML</strong>, <strong>CSS</strong> and <strong>JS</strong>.",
-        "imgLink": "/assets/img/wolfgang-hasselmann--AuNuqFUPQ8-unsplash.jpg",
+        "imgLink": "/assets/img/Nasa-IoTD.png",
         "liveProject": "#",
         "gitLink": "https://github.com/GaborSzaboHUN/ask-nasa-javascript-GaborSzaboHUN"
     },
@@ -35,7 +35,7 @@ const portfolio = [
         "id": "5",
         "title": "Build a Behance UI/UX/Web project",
         "details": "I have created this project to practice UI build. I looked for a nice web design on Behance and built it using <strong>HTML</strong> and <strong>CSS</strong> and added small effects.",
-        "imgLink": "/assets/img/wolfgang-hasselmann-jl3bltYgs8c-unsplash.jpg",
+        "imgLink": "/assets/img/Behance-Sitebuild.png",
         "liveProject": "#",
         "gitLink": "https://github.com/GaborSzaboHUN/behanceProjectW2"
     },
