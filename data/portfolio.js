6fbbee95379c2fe598315740d9be3ea63fd4385d
GaborSzaboHUN <66555347+GaborSzaboHUN@users.noreply.github.com>
2022-12-10 01:17:08 +0100

    portfolio links updated

--- data/portfolio.js
+++ data/portfolio.js
@@ -4,7 +4,7 @@ const portfolio = [
         "title": "Art Gallery with React",
         "details": "With this project my goal was to create a page summarizing selected exhibitions of a fictional gallery. I fetched the data from the public API of the MET museum. This project is made with <strong>HTML</strong>, <strong>CSS</strong> and <strong>React</strong>.",
         "imgLink": "/assets/img/Gagarin-gallery.png",
-        "liveProject": "#",
+        "liveProject": "https://638fe72621c9390a0838ea01--venerable-melomakarona-760513.netlify.app/",
         "gitLink": "https://github.com/GaborSzaboHUN/art-galery-React"
     },
     {
@@ -12,7 +12,7 @@ const portfolio = [
         "title": "Weather app with React",
         "details": "My first weather application made with react. Made as part of a team effort. I wanted to improve the previous JS version with some extra features. This project is made with <strong>HTML</strong>, <strong>CSS</strong> and <strong>React</strong>.",
         "imgLink": "/assets/img/WeatherApp-React.png",
-        "liveProject": "#",
+        "liveProject": "https://gaborszabohun.github.io/weatherAppReact/",
         "gitLink": "https://github.com/GaborSzaboHUN/weather-App-React"
     },
     {
@@ -20,7 +20,7 @@ const portfolio = [
         "title": "Weather app with JS",
         "details": "My first weather app that I made with JS. My goal in the project was to automatically offer results from a list stored in a JSON file in case of a search, and to store the favorites at the top of the existing drop-down list. This project is made with <strong>HTML</strong>, <strong>CSS</strong> and <strong>JS</strong>.",
         "imgLink": "/assets/img/WeatherApp-JS.png",
-        "liveProject": "#",
+        "liveProject": "https://gaborszabohun.github.io/weather-app-JS.github.io/frontend/",
         "gitLink": "https://github.com/GaborSzaboHUN/weather-App-JS"
     },
     {
@@ -28,7 +28,7 @@ const portfolio = [
         "title": "Image of the day by NASA",
         "details": "In this project, I display the image of the day by NASA with <strong>fetch</strong>. Using the calendar, you can also view photos and videos from previous days. This project is made with <strong>HTML</strong>, <strong>CSS</strong> and <strong>JS</strong>.",
         "imgLink": "/assets/img/Nasa-IoTD.png",
-        "liveProject": "#",
+        "liveProject": "https://gaborszabohun.github.io/img-of-the-day-nasa.github.io/frontend/",
         "gitLink": "https://github.com/GaborSzaboHUN/ask-nasa-javascript-GaborSzaboHUN"
     },
     {
